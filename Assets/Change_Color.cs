fee503936f20caceff65baba75b02d083b74fc92
PauCastilla01 <paulinacastillapadilla@comunidad.unam.mx>
2025-03-09 12:31:33 -0600

    Numeros Randoms Hexadecimal
    Agregue implementación para que se seleccionaran de forma aleatoria los colores.

--- Assets/Change_Color.cs
+++ Assets/Change_Color.cs
@@ -9,21 +9,28 @@ public class Change_Color : MonoBehaviour
 
     // Lista de colores en formato hexadecimal
     private string[] hexColors = { "#FF8585", "#FFA07A", "#FFE680", "#98FB98", "#64C8FF", "#A68DFF", "#FFCCE5" };
-    private int currentIndex = 0; // Índice del color actual
+    private int lastIndex = -1; // Último índice usado
 
     public void ChangeColor_BTN()
     {
+        int newIndex;
+
+        // Asegurar que el nuevo color sea diferente al anterior
+        do
+        {
+            newIndex = Random.Range(0, hexColors.Length);
+        } while (newIndex == lastIndex);
+
+        lastIndex = newIndex; // Guardar el nuevo índice
+
         // Convertir el color hexadecimal a Color
-        Color newColor;
-        if (ColorUtility.TryParseHtmlString(hexColors[currentIndex], out newColor))
+        if (ColorUtility.TryParseHtmlString(hexColors[newIndex], out Color newColor))
         {
             // Aplicar el nuevo color al modelo y al moño
             model.GetComponent<Renderer>().material.color = newColor;
             moño.color = newColor;
         }
-
-        // Cambiar al siguiente color en la lista
-        currentIndex = (currentIndex + 1) % hexColors.Length;
     }
 }
+
 
---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVk5UWxQUC8vLy8vQUFBQm1SeTVyaU5pU1VqY2x3UDF0Nk1wQUkwR01TWmVYRjBJS2M5RG9VK1RrbSs3eW56WWxFSkZVMmJtWUY0QS9pWnRWMHBQays2VWxPWUEvczVFdGF6VjBSS2FldC9LSHNPS25IT3NDVnorQVcySmljVXQ5L2NRZCtZNk1PeEszLzhlbE42Y3k4bjBhcUcyQW4rNDdzOENWeEdLeEdCUTc5M25CUUQ1ODgvamk3cTVtR1pUVU1ZelRzczFKaXZ2Y1oxRjJPbGMyRGRoTWtwVldIS3l4TmcrMTREMEhJVUg0NEpYSUwyeFVCb3J2SlpwR0Y5VDdqdHJtOVBJcjJGb2JWTFpTV3VSaE8wRGtweHZ5RW9UL1hTdzhJcGJkNTF0REhncGJ3TzdNazh0dldyakdlbzN6OXZGNURwdlVzci82K1lQKzh0QzhMTDU5bnVuVm9oVkpuOWlIdC81N05VUmFKWGFNaVEzK014dHRPamc=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
